0ec818e5c4932018072cbd3a94e66d266e28e796
Akul Santhosh <akul753@gmail.com>
2018-12-29 18:08:37 +0530

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -11,80 +11,80 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 
-function SwapDivsWithClick(div1,div2)
-{
-   d1 = document.getElementById(div1);
-   d2 = document.getElementById(div2);
-   if( d2.style.display == "none" )
-   {
-      d1.style.display = "none";
-      d2.style.display = "block";
-   }
-   else
-   {
-      d1.style.display = "block";
-      d2.style.display = "none";
-   }
+function SwapDivsWithClick(div1, div2) {
+    d1 = document.getElementById(div1);
+    d2 = document.getElementById(div2);
+    if (d2.style.display == "none") {
+        d1.style.display = "none";
+        d2.style.display = "block";
+    }
+    else {
+        d1.style.display = "block";
+        d2.style.display = "none";
+    }
 }
 
 
-function register(){
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var name = document.getElementById("name").value;
-  var college = document.getElementById("college").value;
-  var phone = document.getElementById("phone").value;
-  var gender = document.getElementById("gender").value;
+function register() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var name = document.getElementById("name").value;
+    var college = document.getElementById("college").value;
+    var phone = document.getElementById("phone").value;
+    var gender = document.getElementById("gender").value;
 
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode)
-  });
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
 
-  if(firebase.auth().currentUser){
-    var userId = firebase.auth().currentUser.uid;
-    database.ref('users/' + userId).set({
-    name: name,
-    email: email,
-    college: college,
-    phone: phone,
-    gender: gender
-  });
+        user = firebase.auth().currentUser;
+        if (user) {
+            // User is signed in.
+            var userId = user.uid;
+            database.ref('users/' + userId).update({
+                name: name,
+                email: email,
+                college: college,
+                phone: phone,
+                gender: gender
+            }).then(function () {
+                // write what happens after registration is compete
+                alert("registration succesfull")
 
-  alert("registration successfull")
-} else {
-  alert("registration failed try again")
+
+            });
+        }
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        alert("registration failed try again")
+    });
 }
 
-}
+function login() {
+    var email = document.getElementById("emailin").value;
+    var password = document.getElementById("passwordin").value;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
 
-function login(){
-  var email = document.getElementById("emailin").value;
-  var password = document.getElementById("passwordin").value;
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
 
-  var errorCode = error.code;
-  var errorMessage = error.message;
-
-});
+    });
 
 }
 
-function forget(){
+function forget() {
     var email = document.getElementById("emailfor").value;
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
+    firebase.auth().sendPasswordResetEmail(email).then(function () {
         alert('Password Reset Email Sent!');
-      }).catch(function(error) {
+    }).catch(function (error) {
         var errorCode = error.code;
         var errorMessage = error.message;
         if (errorCode == 'auth/invalid-email') {
-          alert(errorMessage);
+            alert(errorMessage);
         } else if (errorCode == 'auth/user-not-found') {
-          alert(errorMessage);
+            alert(errorMessage);
         }
         console.log(error);
-
-      });
+    });
 }
